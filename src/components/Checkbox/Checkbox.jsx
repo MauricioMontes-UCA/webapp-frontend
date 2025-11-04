bb39fa3b55e09a415786be6b216d443e18f7b06c
xochillmiguel <00114924@uca.edu.sv>
2025-11-04 16:14:25 -0600

    Últimos ajustes antes de push

--- src/components/Checkbox/Checkbox.jsx
+++ src/components/Checkbox/Checkbox.jsx
@@ -3,7 +3,7 @@ import "./Checkbox.css";
 
 export default function Checkbox({ label, checked, onChange }) {
   return (
-    <label className="checkbox-container">
+    <label className="checkbox">
       <input type="checkbox" checked={checked} onChange={onChange} />
       <span>{label}</span>
     </label>
--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -3,29 +3,27 @@ import "./InputField.css";
 import eyeIcon from "../../assets/icons/eye.svg";
 import eyeClosedIcon from "../../assets/icons/eye-closed.svg";
 
-export default function InputField({ label, type, placeholder, value, onChange }) {
+export default function InputField({ label, type, placeholder, value, onChange, list }) {
   const [showPassword, setShowPassword] = useState(false);
-
   const isPassword = type === "password";
   const inputType = isPassword && showPassword ? "text" : type;
 
   return (
-    <div className="input-container">
+    <div className='input-field'>
       <label>{label}</label>
-
-      <div className="input-wrapper">
+      <div className='input-wrapper'>
         <input
           type={inputType}
           placeholder={placeholder}
           value={value}
           onChange={onChange}
+          list={list} 
         />
-
         {isPassword && (
           <img
             src={showPassword ? eyeClosedIcon : eyeIcon}
-            alt="toggle password visibility"
-            className="toggle-eye"
+            alt='Ver contraseña'
+            className='toggle-eye'
             onClick={() => setShowPassword(!showPassword)}
           />
         )}
