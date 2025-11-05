0b4b5e37f93c339f45a4515862e87b0821c9c6e4
xochillmiguel <00114924@uca.edu.sv>
2025-11-04 20:12:25 -0600

    feat: configuración de rutas y conexión entre homepage y login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Homepage from './pages/Home'
 import Login from './pages/Login/Login'
 import './App.css'
 
 function App() {
   return (
-    <div className="app">
-      <Homepage />
-      <Login/>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   )
 }
 
