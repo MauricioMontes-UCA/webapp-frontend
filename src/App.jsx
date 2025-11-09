3368b028d1700c62be22dfc37ab35c2a3c7cb386
DeniseMaldonadoG <00133523@uca.edu.sv>
2025-11-08 20:20:27 -0600

    Bookspage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; 
+
 import Homepage from './pages/Home'
 import Login from './pages/Login/Login'
+import Description from './pages/Books-page/Description'
+
+import Header from './components/Header/Header'
 import './App.css'
 
 function App() {
@@ -10,9 +17,19 @@ function App() {
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
+        
+        <Route
+          path="/description"
+          element={
+            <>
+              <Header />
+              <Description />
+            </>
+          }
+        />
       </Routes>
     </Router>
   )
 }
 
-export default App;
+export default App
